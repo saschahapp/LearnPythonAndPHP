6936f6d29a5e7e25c2d35d28cf459d18f1cec790
saschahapp <sascha.happ@leenmann.de>
2020-09-17 13:38:04 +0200

    new

--- test.py
+++ test.py
@@ -36,11 +36,12 @@ wert3 = int(wert1) + int(wert2)
 print ("Erster + zweiter Wert ist:",wert3)
 inhalt = eval(input("Geben Sie einen beliegen Wet ein:"))
 print ("Datentyp des Wertes:", type(inhalt))
-"""
-fahrzeug1 = ["VW", "Golf", 2011, 5000]
+
+fahrzeug1 = ["VW", "Golf", 2011, 5000] # Liste!
 fahrzeug2 = ["BMW", "M3", 2020, 100000]
 listefahrzeuge = [fahrzeug1, fahrzeug2]
 fahrzeug3 = {"Marke": "VW", "Modell": "Golf", "Baujahr": 2012, "Preis": 5000}
+fahrzeug3["farbe"] = "blau" # hinzufügen der Liste
 print (fahrzeug3)
 print (fahrzeug3["Marke"])
 del fahrzeug3["Baujahr"]
@@ -49,3 +50,68 @@ print (list (fahrzeug3.keys()))
 print (sorted (fahrzeug3.keys()))
 fahrzeug1 = dict(marke="VW", model="Golf", baujahr=2012, preis=5000) # geht nur wenn Schlüssel string ist!!
 print (fahrzeug1["marke"])
+fahrzeug1 = "VW", "Golf", 2011, 5000 # Tupel!
+tu = "blau", # neue Tupel erstellen
+fahrzeug1 = fahrzeug1 + tu # hinzufügen der neuen Tupel
+print (fahrzeug1)
+fahrzeug1 = "VW", "Golf", 2011, [5000]
+fahrzeug1[3][0] = 4500
+print (fahrzeug1)
+print ("Programm für einen Buchhändler")
+print ("\n")
+buch1 = ["Titel", "Autor", "Artikelnummer", "Preis"]
+buch2 = ["Titel2", "Autor2", "Artikelnummer2", "Preis2"]
+buch3 = ["Titel3", "Autor3", "Artikelnummer3", "Preis3"]
+print (buch1[0])
+print (buch1[1])
+print (buch1[2])
+print (buch1[3])
+print ("\n")
+print (buch2[0])
+print (buch2[1])
+print (buch2[2])
+print (buch2[3])
+print ("\n")
+print (buch3[0])
+print (buch3[1])
+print (buch3[2])
+print (buch3[3])
+print ("Programm für einen Buchhändler")
+print("\n")
+buch1 = {"Titel": "Titel", "Autor": "Autor", "Artikelnummer": "Artikelnummer", "Preis": "Preis"}
+buch2 = {"Titel": "Titel2", "Autor": "Autor2", "Artikelnummer": "Artikelnummer2", "Preis": "Preis2"}
+buch3 = {"Titel": "Titel3", "Autor": "Autor3", "Artikelnummer": "Artikelnummer3", "Preis": "Preis3"}
+print (buch1["Titel"])
+print (buch1["Autor"])
+print (buch1["Artikelnummer"])
+print (buch1["Preis"])
+print ("\n")
+print (buch2["Titel"])
+print (buch2["Autor"])
+print (buch2["Artikelnummer"])
+print (buch2["Preis"])
+print ("\n")
+print (buch3["Titel"])
+print (buch3["Autor"])
+print (buch3["Artikelnummer"])
+print (buch3["Preis"])
+print ("Programm für einen Buchhändler")
+print("\n")
+buch1 = "Titel", "Autor", "Artikelnummer", "Preis"
+buch2 = "Titel2", "Autor2", "Artikelnummer", "Preis2"
+buch3 = "Titel3", "Autor3", "Artikelnummer3", "Preis3"
+print (buch1[0])
+print (buch1[1])
+print (buch1[2])
+print (buch1[3])
+print ("\n")
+print (buch2[0])
+print (buch2[1])
+print (buch2[2])
+print (buch2[3])
+print ("\n")
+print (buch3[0])
+print (buch3[1])
+print (buch3[2])
+print (buch3[3])
+"""
