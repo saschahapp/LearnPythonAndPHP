1fb212aa3b85175806b9438e397dd7fe8ed15977
saschahapp <sascha.happ@leenmann.de>
2020-09-21 07:36:56 +0200

    new

--- oop.py
+++ oop.py
@@ -82,6 +82,77 @@ print (suv1.getAllradantrieb())
 print (suv1.getMarke())
 
 
+# Aufgabe 1:
+class Wohnung:
+
+    def __init__(self, na, wo, st, an, pr):
+
+        self.__Namen = na
+        self.__Wohnung = wo
+        self.__Standort = st
+        self.__AnzahlBetten = an
+        self.__PreisProUebernachtung = pr
+
+    def getNamen(self):
+        return self.__Namen
+
+    def getWohnung(self):
+        return self.__Wohnung
+
+    def getStandort(self):
+        return self.__Standort
+
+    def getAnzahlBetten(self):
+        return self.__AnzahlBetten
+
+    def getPreis(self):
+        return self.__PreisProUebernachtung
+
+    def setNamen(self, namen_neu):
+        namen_neu = self.__Namen
+
+    def setBetten(self, betten_neu):
+        betten_neu = self.__AnzahlBetten
+    
+    def setPreis(self, preis_neu):
+        preis_neu = self.__PreisProUebernachtung
+
+
+# Hauptprogramm:
+
+kunde1 = Wohnung("Peter Neumeier", "Wohnung 12", "Auerbach", 2, 99)
+kunde2 = Wohnung("Max Karlstetter", "Wohnung 11", "Auerbach", 1, 79)
+kunde3 = Wohnung("Daniel Flughafen", "Wohnung 9", "Auerbach", 3, 129)
+
+print ( kunde1.getNamen(), kunde2.getNamen(), kunde3.getNamen())
+print (kunde1.getWohnung(), kunde2.getWohnung(), kunde3.getWohnung())
+print (kunde1.getStandort(), kunde2.getStandort(), kunde3.getStandort())
+print (kunde1.getAnzahlBetten(), kunde2.getAnzahlBetten(), kunde3.getAnzahlBetten())
+print (kunde1.getPreis(), kunde2.getPreis(), kunde3.getPreis())
+
+
+#Aufgabe2:
+
+class Kombi(Auto):
+
+    def __init__(self, ma, mo, bj, pr, ko):
+
+        Auto.__init__(self, ma, mo, bj, pr)
+        self.__Kofferrauminhalt = ko
+
+    def getKofferraum(self):
+        return self.__Kofferrauminhalt
+
+
+auto4 = Kombi("BMW", "M335", 2020, 49000, "Leer")
+print (auto4.getMarke(), auto4.getModell(), auto4.getBaujahr(), auto4.getPreis(), auto4.getKofferraum())
+
+
+
+        
+        
+
+
 
 
 
